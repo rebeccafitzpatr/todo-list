89ed7ab1cf4bdf50fa0a5d7bbbba49422b6ffb3a
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2024-11-21 11:30:14 +1300

    bug fix BASE URL

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -15,7 +15,7 @@ const TodoList = () => {
 
     queryFn: async ()=> {
       try {
-        const res = await fetch("http://localhost:5000/api/todos");
+        const res = await fetch("BASE_URL + `/todos`");
         const data = await res.json();
 
         if (!res.ok) {
