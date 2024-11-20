bb5993c1e7eda58b6d9fa5d9325f7f95d847fe2e
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2024-11-21 11:16:59 +1300

    added all CRUD functionality to React app

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -1,30 +1,35 @@
 import { Flex, Spinner, Stack, Text } from "@chakra-ui/react";
-import { useState } from "react";
 import TodoItem from "./TodoItem";
+import { useQuery } from "@tanstack/react-query";
+
+export type Todo = {
+  _id: number;
+  body: string;
+  completed: boolean;
+};
+
 const TodoList = () => {
-	const [isLoading, setIsLoading] = useState(false);
-	const todos = [
-		{
-			_id: 1,
-			body: "Buy groceries",
-			completed: true,
-		},
-		{
-			_id: 2,
-			body: "Walk the dog",
-			completed: false,
-		},
-		{
-			_id: 3,
-			body: "Do laundry",
-			completed: false,
-		},
-		{
-			_id: 4,
-			body: "Cook dinner",
-			completed: true,
-		},
-	];
+
+  const {data:todos, isLoading} = useQuery<Todo[]>({
+    queryKey: ["todos"],
+
+    queryFn: async ()=> {
+      try {
+        const res = await fetch("http://localhost:5000/api/todos");
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong!");
+        }
+
+        return data || [];
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  })
+
+
 	return (
 		<>
 			<Text fontSize={"4xl"} textTransform={"uppercase"} fontWeight={"bold"} textAlign={"center"} my={2}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,17 @@ import { ChakraProvider } from "@chakra-ui/react"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import theme from "./chakra/theme";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
